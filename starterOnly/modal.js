9cdb81dc9f004901a67aa37610795c3a23ab3884
Othmane <othmane.abouelyzza@gmail.com>
2021-01-17 21:58:23 +0100

    Fixed issue number 2 (Form validation)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,13 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeSpan = document.querySelector(".close");
+const form = document.getElementById("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const quantity = document.getElementById("quantity");
+const checkboxes = document.querySelectorAll('input[name="location"]');
+const conditionsCheckbox = document.getElementById("conditions");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,4 +34,56 @@ closeSpan.addEventListener("click", closeModal);
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+function validate(e) {
+  let valid = true;
+  let checkedLocations = [];
+
+  checkboxes.forEach(function (c) {
+    if (c.checked) {
+      checkedLocations.push(c.value);
+    }
+  });
+
+  if (!firstName.value || firstName.value.length < 2) {
+    valid = false;
+    console.log("firstname invalid");
+  }
+
+  if (!lastName.value || lastName.value.length < 2) {
+    valid = false;
+    console.log("lastName invalid");
+  }
+
+  if (!validateEmail(email.value)) {
+    valid = false;
+    console.log("email invalid");
+  }
+
+  if (!quantity.value || isNaN(quantity.value)) {
+    valid = false;
+    console.log("quantity invalid");
+  }
+
+  if (checkedLocations.length === 0) {
+    valid = false;
+    console.log("checkedLocations invalid");
+  }
+
+  if (!conditionsCheckbox.checked) {
+    valid = false;
+    console.log("conditionsCheckbox invalid");
+  }
+
+  if (!valid) {
+    console.log("Invalid!");
+    e.preventDefault();
+  }
+}
+
+// Email validation function
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
